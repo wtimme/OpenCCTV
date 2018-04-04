46dc07900b1bdc69e0be43a8dd4abd1e1c9e0ef2
wtimme <wtimme@users.noreply.github.com>
2018-04-04 18:36:57 +0200

    Initial commit

---
+++ OpenStreetMapCompletionist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenStreetMapCompletionist
+//
+//  Created by Wolfgang Timme on 4/4/18.
+//  Copyright © 2018 Wolfgang Timme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
