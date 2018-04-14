ccfcd85d4e8257993449dc2a02c0fba8f6d91c21
wtimme <wtimme@users.noreply.github.com>
2018-04-14 16:23:25 +0200

    Remove unused code

--- OpenStreetMapCompletionist/View Controllers/TagListTableViewController.swift
+++ OpenStreetMapCompletionist/View Controllers/TagListTableViewController.swift
@@ -20,12 +20,6 @@ class TagListTableViewController: UITableViewController, UISearchResultsUpdating
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-
         searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.placeholder = "Search Key/Value"
