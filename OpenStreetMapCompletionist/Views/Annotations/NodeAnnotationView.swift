5df0d7c60999d94459731e4dab39db944442e4f6
wtimme <wtimme@users.noreply.github.com>
2018-04-17 01:00:18 +0200

    Make the annotation views less ugly

--- OpenStreetMapCompletionist/Views/Annotations/NodeAnnotationView.swift
+++ OpenStreetMapCompletionist/Views/Annotations/NodeAnnotationView.swift
@@ -16,14 +16,14 @@ class NodeAnnotationView: MKAnnotationView {
 
     override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
         super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
-
-        image = UIImage(bgIcon: .mapicons(.squarePin),
-                        bgTextColor: .annotationBackgroundColor,
-                        bgBackgroundColor: .annotationBackgroundColor,
-                        topIcon: .mapicons(.bicycling),
-                        topTextColor: .annotationForegroundColor,
-                        bgLarge: true,
+        
+        image = UIImage(icon: .mapicons(MapiconsType.bicycling),
                         size: CGSize(width: NodeAnnotationView.annotationDimension, height: NodeAnnotationView.annotationDimension))
+        
+        backgroundColor = UIColor(red: 57/255, green: 42/255, blue: 89/255, alpha: 0.1)
+        layer.cornerRadius = 4.0
+        layer.borderColor = UIColor(white: 0.1, alpha: 0.8).cgColor
+        layer.borderWidth = 0.5
     }
 
     required init?(coder _: NSCoder) {
